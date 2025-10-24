2cba20b9f94cf094985156507b0ee3eeee796ce7
satvik-adeptmind <satvik@adeptmind.ai>
2025-10-24 14:32:28 +0530

    added size in  jacamo

--- src/retailers/jacamo_parser.py
+++ src/retailers/jacamo_parser.py
@@ -12,12 +12,13 @@ class JacamoParser(BaseParser):
             color = product.get('color', 'N/A').strip() or 'N/A'
             material = product.get('material', 'N/A').strip() or 'N/A'
             pattern = product.get('pattern', 'N/A').strip() or 'N/A'
-        
+            size = product.get('size', 'N/A').strip() or 'N/A'
             llm_texts.append(f"""prod {i + 1}:
 title: {title}
 description: {desc}
 color: {color}
 material: {material}
 pattern: {pattern}
+size: {size}
 """)
         return self._format_llm_output(search_keyword, llm_texts)

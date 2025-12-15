e89ef433f382032bf84b7478b7972f555335929a
satvik-adeptmind <satvik@adeptmind.ai>
2025-12-15 16:03:33 +0530

    removed desc solesupp v2

--- src/retailers/solesupplierv2_parser.py
+++ src/retailers/solesupplierv2_parser.py
@@ -8,9 +8,9 @@ class SoleSupplierV2(BaseParser):
         llm_texts = []
         for i, product in enumerate(products):
             title = product.get('title', 'N/A').strip() or 'N/A'
-            desc = product.get('description', 'N/A').strip() or 'N/A'
+            #desc = product.get('description', 'N/A').strip() or 'N/A'
         
             llm_texts.append(f"""prod {i + 1}:
 title: {title}
-description: {desc}""")
+""")
         return self._format_llm_output(search_keyword, llm_texts)
